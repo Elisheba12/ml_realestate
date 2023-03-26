e48a80645fe7a9c9e1299af6e0d3a07e44d35968
Elizabeth Salako <waleade602@gmail.com>
2023-03-26 08:26:31 -0500

    modules

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/layout';
+
+const Footer = () => (
+  <Box textAlign='center' p='5' color='blue.600' borderTop='1px' borderColor='gray.300'>
+    © 2021 ML Realtor, Inc.
+  </Box>
+);
+
+export default Footer;
